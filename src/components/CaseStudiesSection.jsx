ad78df65993ef9e71534974476d6140fdd9fc302
anmol37082 <singanmol714@gmail.com>
2026-05-19 10:50:18 +0530

    Updated UI design

--- src/components/CaseStudiesSection.jsx
+++ src/components/CaseStudiesSection.jsx
@@ -10,7 +10,7 @@ const caseStudies = [
     title: "Air Pollution Studies",
     description: "Detect heavy metals and urban toxins",
     image:
-      "https://images.unsplash.com/photo-1598758448707-20609c80cca9?auto=format&fit=crop&fm=jpg&q=80&w=1600",
+      "https://images.unsplash.com/photo-1564874997645-eba9f36e5bf2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     year: "2025",
